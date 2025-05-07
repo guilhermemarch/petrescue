8196c349908665ecb957c557140fb17377efdb5b
Guilherme Marschall <c0mbedforn1ght@gmail.com>
2025-05-07 11:13:13 -0300

    removing lombok and adding the methods

--- src/main/java/com/petrescue/dto/auth/AuthResponse.java
+++ src/main/java/com/petrescue/dto/auth/AuthResponse.java
@@ -1,10 +1,6 @@
 package com.petrescue.dto.auth;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
-@Data
-@AllArgsConstructor
 public class AuthResponse {
     private String token;
     private String email;
@@ -12,4 +8,42 @@ public class AuthResponse {
     private String role;
 
 
-} 
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public AuthResponse(String token, String email, String name, String role) {
+        this.token = token;
+        this.email = email;
+        this.name = name;
+        this.role = role;
+    }
+}
