346219dc09f75a3e6b60b4160fd2760b0d705d72
Guilherme Marschall <c0mbedforn1ght@gmail.com>
2025-05-08 10:47:05 -0300

    fix cloudinary config to store images send by users

--- src/main/java/com/petrescue/config/CloudinaryConfig.java
+++ src/main/java/com/petrescue/config/CloudinaryConfig.java
@@ -4,11 +4,13 @@ import com.cloudinary.Cloudinary;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@Profile("!test")
 public class CloudinaryConfig {
 
     @Value("${cloudinary.cloud-name}")
